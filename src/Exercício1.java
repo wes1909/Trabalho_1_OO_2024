030a8f259ef44f48eac6b05d2cadf5423fbae1dc
wes1909 <wesleipereira721@gmail.com>
2024-05-21 19:29:09 -0300

    Exercício_2

--- src/Exercício1.java
+++ src/Exercício1.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Exercício1 {
+import java.util.Scanner;
+
+public class Exercício1 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int num;
+
+
+        System.out.print("Entre com um número inteiro:");
+            num = scan.nextInt();
+
+        System.out.println("O Sucessor de "+num+ " é "+(num+1));
+        System.out.println("O Antecessor de "+num+ " é "+(num-1));
+    }
 }
--- src/Exercício2.java
+++ src/Exercício2.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Exercício2 {
+import java.util.Scanner;
+
+public class Exercício2 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int km, gas, media;
+
+
+        System.out.print("Informe a distância percorrida: (km) ");
+            km = scan.nextInt();
+
+        System.out.print("Agora informe o total de combustível usado: (Litros) ");
+            gas = scan.nextInt();
+
+                 media = km/gas;
+
+        System.out.println("**O consumo médio de seu veículo é de "+media+ " km/L**");
+
+    }
+
 }
