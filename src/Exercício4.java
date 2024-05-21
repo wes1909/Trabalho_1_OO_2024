ecab49d6a4ba541899331336529d91d5a8a9cd60
wes1909 <wesleipereira721@gmail.com>
2024-05-21 19:42:33 -0300

    Exercício4

---
+++ src/Exercício4.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Exercício4 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String sex;
+
+
+        System.out.print("Insira o seu sexo (M/F): ");
+            sex = scan.next().toUpperCase();
+
+        if (sex.equals("M")) {
+            System.out.println("Você é Masculino");
+
+        } else if (sex.equals("F")) {
+
+            System.out.println("Você é Feminino ");
+
+        } else {
+
+            System.out.println("Sexo Inexistente");
+        }
+
+    }
+}
